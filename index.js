9b3abd290b3bad056fe29944cacc569f40047ac2
Asad Robinson <juebreed@gmail.com>
2019-06-26 14:05:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ whisper("HELLO");
 function logShout (string) {
   console.log('HELLO');
 }
+logShout("hello");
 
 function logWhisper (string) {
   console.log('hello');
 }
-whisper("HELLO");
+logWhisper("HELLO");
 
 function sayHiToGrandma (string) {
   if (string === string.toLowerCase())
