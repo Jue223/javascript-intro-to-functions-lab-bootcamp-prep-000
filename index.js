bc92554554cbdf2c996811eda34ce70fb015c3a5
Asad Robinson <juebreed@gmail.com>
2019-06-26 13:27:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
 function shout (string) {
   return string.toUpperCase() //'HELLO!'
 }
-function whisper (string) {
-  return string.toLowerCase()
-}
 
-function logShout (hello) {
- console.log ('HELLO');
-}
-
-function logWhisper (hello) {
-  console.log('hello');
-}
-
-function sayHiToGrandma(string) {
-  if ((string)===toUpperCase) {
-    return 'YES INDEED!'
-  }
-  else ((string)===toLowerCase) {
-    return "I can't hear you!"
-  }
-  else if ((string)==="I love you, Grandma.") {
-    return "I love you, too."
-  }
-  }
 
 
